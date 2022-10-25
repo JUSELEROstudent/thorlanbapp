5ffa97f6bcb6e4bbb5da0fce0b05247b55ca7723
JUSELERO <juan.leon7@correo.uis.edu.co>
2022-10-24 22:44:54 -0500

    primer comentario api .net core

---
+++ GotsThorlabs/NodesApi/NodeGenerics.cs
@@ -0,0 +1,22 @@
+﻿namespace GotsThorlabs.NodesApi
+{
+    public class NodeGenerics
+    {
+        public NodeGenerics(WebApplication App)
+        {
+
+            // Getters de las peticiones.
+            App.MapGet("/hola", () => 
+            {
+                return "hola persona nueva";
+                }
+            );
+
+            App.MapGet("/1", () =>
+            {
+                return "un numero devulelto 1";
+            }
+            );
+        }
+    }
+}
