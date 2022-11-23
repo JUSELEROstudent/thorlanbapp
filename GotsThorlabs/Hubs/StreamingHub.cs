c85bf49717cc4577051e409a7aa06845d55e7851
JUSELERO <juan.leon7@correo.uis.edu.co>
2022-11-22 22:19:33 -0500

    SignalR implementacion
    se crea el mecanismo de suscripción desde front al hub de signalR asignado

--- GotsThorlabs/Hubs/StreamingHub.cs
+++ GotsThorlabs/Hubs/StreamingHub.cs
@@ -3,13 +3,9 @@ using System.Runtime.CompilerServices;
 
 namespace GotsThorlabs.Hubs
 {
-    public class ChatHub : Hub
+    public class StreamingHub : Hub
     {
-        public async Task SendMessage(string user, string message)
-        {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
-        }
-        public async IAsyncEnumerable<string> Counter(
+        public async IAsyncEnumerable<resourcesignal> Counter(
          int count,
          int delay,
          [EnumeratorCancellation]
@@ -17,16 +13,27 @@ namespace GotsThorlabs.Hubs
         {
             for (var i = 0; i < count; i++)
             {
+                var resueta = new resourcesignal
+                {
+                    Name = "Sebastian",
+                    numero = i
+                };
                 // Check the cancellation token regularly so that the server will stop
                 // producing items if the client disconnects.
                 cancellationToken.ThrowIfCancellationRequested();
 
-                yield return "lo que yo quiero";
+                yield return resueta;
 
                 // Use the cancellationToken in other APIs that accept cancellation
                 // tokens so the cancellation can flow down to them.
                 await Task.Delay(delay, cancellationToken);
             }
         }
+    }
+
+    public class resourcesignal { 
+        public string Name { get; set; }
+        // public CancellationToken cancelacion { get; set; }
+        public int numero { get; set; }
     }
 }
