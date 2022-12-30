9082e150fd7fc1a4ab6fdcb3464bb4f5d8315c8c
JUSELERO <juan.leon7@correo.uis.edu.co>
2022-12-30 00:17:12 -0500

    Se consigue una version valida de la dll Presentationframework
    lo que permite crear la coneccion con los motores pero en el metodo implementado en la funcion aparece un error ruego que sea tambien corregible.

--- GotsThorlabs/NodesApi/NodeHomepage.cs
+++ GotsThorlabs/NodesApi/NodeHomepage.cs
@@ -1,20 +1,42 @@
 ﻿using Thorlabs.MotionControl.DeviceManagerCLI;
 using Thorlabs.MotionControl.KCube.InertialMotorCLI;
+using Thorlabs.MotionControl.GenericMotorCLI;
 using AForge.Video.DirectShow;
 using AForge.Video;
 using System.Drawing;
 using OpenCvSharp;
 using System.Diagnostics;
+using System.Net;
+using Thorlabs.MotionControl.Tools.Common;
+using Thorlabs.MotionControl.Tools.Logging;
+
 
 namespace GotsThorlabs.NodesApi
 {
     public class NodeHomepage
     {
         FrameSource frameSource;
+        public static void Move_Method1(KCubeInertialMotor device, InertialMotorStatus.MotorChannels channel, int position)
+        {
+            try
+            {
+                Console.WriteLine("Moving Device to {0}", position);
+                device.MoveTo(channel, position, 60000);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Failed to move to position");
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine("Device Moved");
+        }
+
         public NodeHomepage(WebApplication App)
         {
             App.MapGet("/home/devices", async () =>
             {
+                // SimulationManager.Instance.InitializeSimulations();
                 try
                 {
                     // Tell the device manager to get the list of all devices connected to the computer
@@ -29,9 +51,92 @@ namespace GotsThorlabs.NodesApi
                 }
                 List<string> serialNumbers = DeviceManagerCLI.GetDeviceList(KCubeInertialMotor.DevicePrefix_KIM101);
                 //CargaDispositivos();
-               // Readcameras();
+                // Readcameras
+                //SimulationManager.Instance.UninitializeSimulations();
+
+                KCubeInertialMotor device = KCubeInertialMotor.CreateKCubeInertialMotor(serialNumbers[0]);
+                if (device == null)
+                {
+                    // An error occured
+
+                }
+
+                // Open a connection to the device.
+                try
+                {
+                    device.Connect(serialNumbers[0]);
+                }
+                catch (Exception)
+                {
+                    // Connection failed
+                    serialNumbers.Add("Failed to open device {0}");
+                    return serialNumbers;
+                }
+                if (!device.IsSettingsInitialized())
+                {
+                    try
+                    {
+                        device.WaitForSettingsInitialized(5000);
+                    }
+                    catch (Exception)
+                    {
+                        serialNumbers.Add("Settings failed to initialize");
+                    }
+                }
+                DeviceInfo deviceInfo = device.GetDeviceInfo();
+                Console.WriteLine("Device {0} = {1}", deviceInfo.SerialNumber, deviceInfo.Name);
+                device.StartPolling(250);
+                Thread.Sleep(500);
+                device.EnableDevice();
+                Thread.Sleep(500);
+
+                InertialMotorConfiguration InertialMotorConfiguration = device.GetInertialMotorConfiguration(serialNumbers[0]);
+                ThorlabsInertialMotorSettings currentDeviceSettings = ThorlabsInertialMotorSettings.GetSettings(InertialMotorConfiguration);
+
+                // Set the 'Step' paramaters for the Inertia Motor and download to device
+                currentDeviceSettings.Drive.Channel(InertialMotorStatus.MotorChannels.Channel1).StepRate = 500;
+                currentDeviceSettings.Drive.Channel(InertialMotorStatus.MotorChannels.Channel1).StepAcceleration = 100000;
+                device.SetSettings(currentDeviceSettings, true, true);
+
+                // Zero the device
+                //device.SetPositionAs(InertialMotorStatus.MotorChannels.Channel1, 0);
+
+                int position = 1000;
+                Move_Method1(device, InertialMotorStatus.MotorChannels.Channel1, position);
+                serialNumbers.Add("se ha movido");
+                // or
+                // Move_Method2(device, InertialMotorStatus.MotorChannels.Channel1, position);
+
+                Decimal newPos = device.GetPosition(InertialMotorStatus.MotorChannels.Channel1);
+                Console.WriteLine("Device Moved to {0}", newPos);
+
+                // Tidy up and exit
+                device.StopPolling();
+                device.Disconnect(true);
+
+                Console.ReadKey();
+
+
                 return serialNumbers;
             });
+        }
+
+        public bool builddeviceslist ()
+        {
+            try
+            {
+                // Tell the device manager to get the list of all devices connected to the computer
+                DeviceManagerCLI.BuildDeviceList();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // An error occurred - see ex for details
+                Console.WriteLine("Exception raised by BuildDeviceList {0}", ex);
+                Console.ReadKey();
+                return false;
+
+            }
         }
 
         //public void Readcameras() 
