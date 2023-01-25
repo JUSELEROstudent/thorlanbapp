527cc51707a323e4872fc838906807a28b13b524
JUSELERO <juan.leon7@correo.uis.edu.co>
2023-01-24 23:43:05 -0500

    se crea Joistick css para uso propio
    continuar con la parte de automatización de mapeo

--- UserInterface/thorlabsinterface/src/views/PlayGround.vue
+++ UserInterface/thorlabsinterface/src/views/PlayGround.vue
@@ -1,9 +1,10 @@
 <template>
   <v-card :elevation="5">
   <div class="body">
+    <div class="d-flex">
   <v-form>
+    <v-col cols="12">
     <v-card-title><h1>Move Manually</h1></v-card-title>
-    <v-col cols="11">
     <v-select
     v-model="configdeviceselect.deviceId"
     :items="listdevices" required
@@ -29,6 +30,28 @@
     ></v-select>
   </v-col>
   </v-form>
+  <v-col>
+    <div class="joystick">
+      <table>
+        <tr>
+          <td colspan="3"><div> <div class=" row up">1</div></div> </td>
+        </tr>
+        <tr>
+          <td ><div class="row left">1 </div> </td>
+          <td><div class="center">0 </div></td>
+          <td><div class="row right">1</div></td>
+        </tr>
+        <tr>
+          <td colspan="3"><div class="row down">1</div></td>
+        </tr>
+      </table>
+      <!-- <div class="up">1</div>
+      <div class="left">1 </div>
+      <div right="right">1</div>
+      <div class="down">1</div> -->
+    </div>
+  </v-col>
+  </div>
   <v-btn v-on:click="moverunpaso" v-if="onrequest" color="success"  large block>Move whit configuration</v-btn>
     <div   v-if="!onrequest" ><v-btn disabled>Move whit configuration</v-btn></div>
   </div>
@@ -87,8 +110,67 @@ export default {
 </script>
 
 <style scoped>
-/* .body {
+ .joystick {
+  background:rgb(216, 214, 214);
+  align-items: center;
+ }
+
+ table {
+  width:100%;
+  max-width: 500px;
+  padding: 50px;
   margin: auto;
-  max-width: 50%;
-} */
+ }
+.center {
+  height: 100px;
+  width: 100px;
+  border-radius: 100px;
+  background: black;
+  border: 2px solid #ffffff ;
+  margin: auto;
+}
+.left , .right , .down , .up {
+  background: gray;
+}
+.left{
+  rotate: -90deg;
+}
+.down{
+  rotate: 180deg;
+}
+.right{
+  rotate: 90deg;
+}
+.row{
+  height: 80px ;
+  width: 30px ;
+  position: relative;
+  margin: auto;
+  z-index: 10;
+  cursor: pointer;
+}
+.row::before{
+  z-index: -2;
+  position: absolute;
+  height: 50px ;
+  width: 50px ;
+  background: gray;
+  top: -24px;
+  right: -10px;
+  rotate: 45deg;
+  border-top:2px solid #24724e;
+  border-left:2px solid #ffffff;
+  content: '';
+}
+.row::after{
+  z-index: -1;
+  position: absolute;
+  height: 20px ;
+  width: 80px ;
+  background:rgb(216, 214, 214);
+  top: 2px;
+  right: -24px;
+  content: '';
+}
+
 </style>
