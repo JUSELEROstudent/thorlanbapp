6281ce94720a962f7928120a25abaad28ebc1b26
JUSELERO <juan.leon7@correo.uis.edu.co>
2024-02-15 22:54:42 -0500

    se crea el login con nuevo layout

--- ClientThorlabs/src/layouts/default.vue
+++ ClientThorlabs/src/layouts/default.vue
@@ -22,7 +22,7 @@
             <span v-show="statusbar" class="self-center text-white" >SIGN OUT</span></div>   
         </div>
         <div class="flex flex-col justify-items-center gap-4 mb-4">
-        <div class="flex justify-center align-bottom"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16"><path fill="white" d="M6.75 1.5a.75.75 0 0 0 0 1.5h4.75A1.5 1.5 0 0 1 13 4.5v7a1.5 1.5 0 0 1-1.5 1.5H6.75a.75.75 0 0 0 0 1.5h4.75a3 3 0 0 0 3-3v-7a3 3 0 0 0-3-3zm3.03 5.97l-2.5-2.5a.75.75 0 0 0-1.06 1.06l1.22 1.22H1.75a.75.75 0 0 0 0 1.5h5.69L6.22 9.97a.75.75 0 1 0 1.06 1.06l2.5-2.5a.75.75 0 0 0 0-1.06"/></svg></div>   
+        <div class="flex justify-center align-bottom"><NuxtLink href="/login"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 16 16"><path fill="white" d="M6.75 1.5a.75.75 0 0 0 0 1.5h4.75A1.5 1.5 0 0 1 13 4.5v7a1.5 1.5 0 0 1-1.5 1.5H6.75a.75.75 0 0 0 0 1.5h4.75a3 3 0 0 0 3-3v-7a3 3 0 0 0-3-3zm3.03 5.97l-2.5-2.5a.75.75 0 0 0-1.06 1.06l1.22 1.22H1.75a.75.75 0 0 0 0 1.5h5.69L6.22 9.97a.75.75 0 1 0 1.06 1.06l2.5-2.5a.75.75 0 0 0 0-1.06"/></svg></NuxtLink></div>   
         </div>
     </div>
 
---
+++ ClientThorlabs/src/layouts/login.vue
@@ -0,0 +1,8 @@
+<template>
+<div>
+  test layout
+</div>
+<slot></slot>
+</template>
+<script>
+</script>
